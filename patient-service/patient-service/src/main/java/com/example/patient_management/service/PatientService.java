e37234c44740ce8fcb1cd3ee2410265a9fbd3ac5
Devendune <devendunegi06@gmail.com>
2025-05-17 15:58:30 +0530

    adding custom exceptions

--- patient-service/patient-service/src/main/java/com/example/patient_management/service/PatientService.java
+++ patient-service/patient-service/src/main/java/com/example/patient_management/service/PatientService.java
@@ -3,10 +3,10 @@ package com.example.patient_management.service;
 import com.example.patient_management.DTO.PatientRequestDTO;
 import com.example.patient_management.DTO.PatientResponseDTO;
 import com.example.patient_management.Model.Patient;
+import com.example.patient_management.exceptions.EmailAlreadyExistsException;
 import com.example.patient_management.mapper.PatientMapper;
 import com.example.patient_management.repository.PatientRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -28,6 +28,10 @@ public class PatientService
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO)
     {
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail()))
+        {
+            throw new EmailAlreadyExistsException("A patient already exists "+patientRequestDTO.getEmail());
+        }
         Patient newPatient=patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
     }
