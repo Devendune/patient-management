bcf5a74f92ef8a2f2d8a00151c71478d06356bae
Devendune <devendunegi06@gmail.com>
2025-07-19 18:00:10 +0530

    Added Logic to send events to patient topic

--- patient-service/src/main/java/com/example/patient_management/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/example/patient_management/kafka/KafkaProducer.java
@@ -1,6 +1,33 @@
 package com.example.patient_management.kafka;
 
+import com.example.patient_management.Model.Patient;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
 public class KafkaProducer
 {
+private final KafkaTemplate<String,byte[]>kafkaTemplate;
 
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvetn(Patient patient)
+    {
+        PatientEvent event=new PatientEvent.Builder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .build();
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
